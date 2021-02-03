1ce91b9730d86d16e386ef4b8f72733d21b3ae1b
douglas.martins <douglas.martins@dotz.com>
2021-02-02 22:30:00 -0300

    Enable APIs.

---
+++ gcp/storage-to-bigquery/infrastructure/enable_services.sh
@@ -0,0 +1,9 @@
+gcloud services enable \
+dataflow.googleapis.com \
+composer.googleapis.com \
+cloudbuild.googleapis.com \
+cloudfunctions.googleapis.com \
+storage-component.googleapis.com \
+storage.googleapis.com \
+bigquery.googleapis.com \
+bigquerystorage.googleapis.com
